12eda37b5d31baab364206371b5ec7f20d79ef1e
JsonSept <jasonseptember.za@gmail.com>
2023-11-08 14:56:19 +0200

    finished task3

--- script.js
+++ script.js
@@ -25,15 +25,15 @@ console.log(`The area of the rectangle is 50`)
 let num1 = 10
 let num2 = 4
 let remainder = 10%4
-console.log(remainder)
+console.log('The remainder is',remainder)
 
 
 // 4.Using the variables you created in the previous step, output the results
 // of the division in the following format: “num1 / num2 = x remainder y.” HINT: Backticks
 
 let x = num1 / num2
-console.log(x)
-console.log("num1/num2 = `8` remainder `2`")
+// console.log(x)
+console.log(`num1/num2 = ${x} remainder ${remainder}`)
 // <------------------------------------------------------------------------------------------------------------------------>
 
 
@@ -77,3 +77,91 @@ let js =
 console.log(jay)
 console.log(js)
 // <----------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+// ***********C. Task 3. functions and conditional statements************
+// N.B. READ WITH UNDERSTANDING!!!
+//  1.  Create 3 functions to convert R1000 into dollars, euros and pounds. Take function naming into consideration!
+
+function dollars() {
+    let dollar = 1000/18.41;
+    console.log(`$ ${dollar.toFixed(2)}`);
+}
+dollars()
+
+function euros() {
+    let euro =  1000/19.7;
+    console.log(`€ ${euro.toFixed(2)}`);
+}
+euros()
+
+function pounds() {
+    let pound = 1000/22.63;
+    console.log(`£ ${pound.toFixed(2)}`);
+}
+pounds()
+
+
+//  2.  Create a function that takes a value and calculates the amount entered including VAT (15%), amount entered should be R400.
+//      Then display the message in the console, "The amount after tax is ...".
+function myFunction() {
+    let inputAmount = 400 
+       let tax = 400 * 15/100
+       let end = inputAmount + tax
+       console.log(end)
+
+       
+}
+myFunction()
+
+//  3.  Declare 3 variables 1stNum, 2ndNum, 3rdNum and assign values of 8, 20 and 14 respectfully. Now write a function that compares “num1” and “num2” and 
+//      displays the larger value.
+    firstNum = 8
+    secondNum = 20
+    thirdNum = 14
+
+    function comparison() {
+        if(firstNum>secondNum) {
+            console.log(firstNum +' is larger')
+        }
+        else {
+            console.log(secondNum + ' is larger')
+        }
+    }
+    comparison()
+//  4.  Write a function that determines whether “num1” is odd or even and displays the
+//      result. (Tip: remember the modulus operator %)
+     function modulus() {
+        if ((firstNum % 2) == 0){
+            console.log('firstNum is even')
+          } else {
+            console.log('firstNum is odd')
+          }
+     }
+     modulus()
+       
+    
+    
+//  5.  Next, write a JavaScript conditional statement to sort the three numbers(1stNum, 2ndNum, 3rdNum) from largest to smallest.
+if (firstNum>secondNum && firstNum>thirdNum){
+    if (secondNum > thirdNum) {
+        console.log (firstNum , secondNum , thirdNum);
+    }
+    else {
+        console.log (firstNum , thirdNum , secondNum );
+    }
+}
+if (secondNum>firstNum && secondNum>thirdNum){
+        if (firstNum > thirdNum) {
+            console.log (secondNum , firstNum , thirdNum);
+        }
+        else {
+            console.log ( secondNum , thirdNum , firstNum);
+        }
+    }
+if (thirdNum>firstNum && thirdNum > secondNum){
+            if (firstNum> secondNum) {
+                console.log (thirdNum , firstNum , secondNum);
+            }
+            else {
+                console.log (thirdNum , secondNum , firstNum);
+            }
+}
